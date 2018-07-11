a4c85d9a99bf1f2f4fd03c822d1b4655b1e3056f
wangkai <wangkailong525@163.com>
2018-07-11 09:46:37 +0800

    将分页加到showIndex映射中

--- src/com/sdut/controller/IndexController.java
+++ src/com/sdut/controller/IndexController.java
@@ -16,57 +16,66 @@ public class IndexController {
 
 	@Autowired
 	private ProductService productService;
-	
+
 	@RequestMapping("showIndex")
 	public String showIndex(Model model) {
-		//查询商品
-		List<Products> productList = productService.findProductList();
-		
-		//将查询到的商品在首页面中进行展示
-		model.addAttribute("productList", productList);
-		
-		
-		return "index";
-	}
-	
-	//设置默认访问问题
-	@RequestMapping("/")
-	public String showIndex1(Model model) {
-		//查询商品
-//		List<Products> productList = productService.findProductList();
-		
-		//获取商品的总记录数
+		// 查询商品
+		// List<Products> productList = productService.findProductList();
+		// 获取商品的总记录数
 		Integer count = productService.findCount();
 		System.out.println(count);
-		
-		//创建pageBean对象，设置页面内容
-		PageBean pageBean= new PageBean(8, 1, count);
+
+		// 创建pageBean对象，设置页面内容
+		PageBean pageBean = new PageBean(8, 1, count);
 		System.out.println(pageBean);
-		
-		//调用具有分页功能的查询方法
+
+		// 调用具有分页功能的查询方法
 		List<Products> productList = productService.findProductListPage(pageBean);
-		//将查询到的商品在首页面中进行展示
+
+		// 将查询到的商品在首页面中进行展示
 		model.addAttribute("productList", productList);
-		
-		
+
 		return "index";
 	}
-	
+
+	// 设置默认访问问题
+	@RequestMapping("/")
+	public String showIndex1(Model model) {
+		// 查询商品
+		// List<Products> productList = productService.findProductList();
+
+		// 获取商品的总记录数
+		Integer count = productService.findCount();
+		System.out.println(count);
+
+		// 创建pageBean对象，设置页面内容
+		PageBean pageBean = new PageBean(8, 1, count);
+		System.out.println(pageBean);
+
+		// 调用具有分页功能的查询方法
+		List<Products> productList = productService.findProductListPage(pageBean);
+		// 将查询到的商品在首页面中进行展示
+		model.addAttribute("productList", productList);
+
+		return "index";
+	}
+
 	@RequestMapping("showAdminIndex")
 	public String showAdminIndex(String id) {
 		Products product = productService.findProductById(id);
-		
+
 		return "admin/index";
 	}
+
 	@RequestMapping("showInfoIndex")
 	public String showInfoIndex(String id, Model model) {
-		//查询商品
+		// 查询商品
 		Products product = productService.findProductById(id);
-		
-		//将查询到的商品在首页面中进行展示
+
+		// 将查询到的商品在首页面中进行展示
 		model.addAttribute("product", product);
-		
+
 		return "productinfo";
 	}
-	
+
 }

f337793a1af19433d0745ea1c34f4e669b5c527f
wangkai <wangkailong525@163.com>
2018-07-13 16:50:08 +0800

    提交订单重定向到订单页面

--- src/com/sdut/controller/OrderController.java
+++ src/com/sdut/controller/OrderController.java
@@ -75,7 +75,7 @@ public class OrderController {
 		session.removeAttribute("cart");
 
 		// 结算完成暂时跳转到首页，订单结算功能到此为止
-		return "redirect:showIndex";
+		return "redirect:showOrders";
 		// return "index";
 	}
 
